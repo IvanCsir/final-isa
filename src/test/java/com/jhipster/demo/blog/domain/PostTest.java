7215fa5406c00e02c71be7714d039c706d6c64f2
IvanCsir <ivanfreiberg@gmail.com>
2024-08-28 16:58:11 -0300

    Unitests hechos

--- src/test/java/com/jhipster/demo/blog/domain/PostTest.java
+++ src/test/java/com/jhipster/demo/blog/domain/PostTest.java
@@ -4,20 +4,46 @@ import static com.jhipster.demo.blog.domain.BlogTestSamples.*;
 import static com.jhipster.demo.blog.domain.PostTestSamples.*;
 import static com.jhipster.demo.blog.domain.TagTestSamples.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import com.jhipster.demo.blog.web.rest.TestUtil;
+import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class PostTest {
 
-    // //Test de unidad
-    // private Post post;
-    // @BeforeEach
-    // public void init {
+    //Test de unidad
+    private Post post;
 
-    // }
+    @BeforeEach
+    public void init() {
+        post = new Post();
+        post.setTitle("Prueba");
+        post.setContent("Contenido de prueba");
+        String fechaStr = "2023-12-12T05:00:00Z";
+        Instant fecha = Instant.parse(fechaStr);
+        post.setDate(fecha);
+    }
+
+    //Compruebo que los atributos sean los seteados
+    @Test
+    public void testPost() {
+        assertEquals("Prueba", post.getTitle());
+        assertEquals("Contenido de prueba", post.getContent());
+        String fechaStr = "2023-12-12T05:00:00Z";
+        Instant fecha = Instant.parse(fechaStr);
+        assertEquals(fecha, post.getDate());
+    }
+
+    @Test
+    public void testFecha() {
+        Instant fechaAhora = Instant.now();
+        assertFalse(fechaAhora.equals(post.getDate()));
+    }
 
     @Test
     void equalsVerifier() throws Exception {
